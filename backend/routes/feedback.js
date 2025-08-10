6e34fed27ed5c120f67845533aa84308b295acfd
Mr-Faizan-Asim <mr.faizan.asim@gmail.com>
2025-08-10 23:18:35 +0800

    Deployment of Frontend

--- backend/routes/feedback.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { auth, requireRole } = require('../middlewares/auth');
-const feedbackController = require('../controllers/feedbackController');
-
-// Leave feedback (learner)
-router.post('/:sessionId', auth, requireRole('learner'), feedbackController.leave);
-
-module.exports = router;
